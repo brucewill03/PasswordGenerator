c91ebaa1af8e06af158d9116641abff7070ce148
Bwilliams <brucewill03@yahoo.com>
2021-11-17 19:21:37 -0500

    updated java script

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max-min+1)) + min;
+}
+function generatePassword(lowerCase,upperCase,digits,specialCharacters) {
+  var passwordLength = getRandomInt( 8, 128);
+  console.log(passwordLength);
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var upperCase = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var special = ['@', '!', '#', '$', '%', '^', '&', '*', '(', ')']; 
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+document.getElementById("generateBtn").addEventListener("click", writePassword);
